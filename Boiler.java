9ffea26ea53f8dfb0b37c1a846fed9dd9dc9368d
taurik <taurik@greeny.cs.tlu.ee>
2015-05-04 19:09:06 +0300

    algus

---
+++ Boiler.java
@@ -0,0 +1,16 @@
+public class Boiler
+{
+	public double maht;
+	public double temperatuur;
+	public Boiler(double maht, double temperatuur)
+	{
+		this.maht = maht; this.temperatuur = temperatuur;
+	}
+	
+	public void kyta(double vatid, double sekundid)
+	{
+		double dzaulid = vatid * sekundid;
+		temperatuur+=dzaulid/4.19/1000;
+	}
+	
+}
